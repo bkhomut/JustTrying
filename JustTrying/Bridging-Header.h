7c3e68e337da7e49ded3b84edfbc5dff74a015ea
bkhomut <boris@Boriss-MacBook-Air.local>
2015-01-27 16:01:51 -0500

    first commit

---
+++ JustTrying/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  JustTrying
+//
+//  Created by Boris Khomut on 1/27/15.
+//  Copyright (c) 2015 Boris. All rights reserved.
+//
+
+#ifndef JustTrying_Bridging_Header_h
+#define JustTrying_Bridging_Header_h
+
+
+#endif
